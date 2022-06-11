38742b74ad74700c6be7c30215b355f4c59e88ca
冯明达 <fengmingda@hello-alpha.com>
2022-06-11 14:46:28 +0800

    【开发】提交20以内数学题

---
+++ Assets/FileFormat/Editor/Excel4Unity/ExcelHelper.cs
@@ -0,0 +1,64 @@
+﻿using OfficeOpenXml;
+using System.IO;
+using OfficeOpenXml.Style;
+using UnityEngine;
+
+public class ExcelHelper
+{
+
+    public static Excel LoadExcel(string path)
+    {
+        Debug.Log("LoadExcel" + path);
+        FileInfo file = new FileInfo(path);
+        ExcelPackage ep = new ExcelPackage(file);
+        Excel xls = new Excel(ep.Workbook);
+        return xls;
+    }
+
+	public static Excel CreateExcel(string path) {
+		ExcelPackage ep = new ExcelPackage ();
+		ep.Workbook.Worksheets.Add ("sheet");
+		Excel xls = new Excel(ep.Workbook);
+		SaveExcel (xls, path);
+		return xls;
+	}
+
+    public static void SaveExcel(Excel xls, string path)
+    {
+        FileInfo output = new FileInfo(path);
+        ExcelPackage ep = new ExcelPackage();
+        for (int i = 0; i < xls.Tables.Count; i++)
+        {
+            ExcelTable table = xls.Tables[i];
+            ExcelWorksheet sheet = ep.Workbook.Worksheets.Add(table.TableName);
+            sheet.PrinterSettings.RightMargin = 1M / 2.54M;
+            sheet.PrinterSettings.LeftMargin = 1M / 2.54M;
+            var widthArr = new[]
+                { 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, 5, 3, };
+            var heightArr = new[]
+                { 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25,25, 25, 25, 25, 25, 25,25, 25, 25, 25, 25, 25,25,25, 25,25};
+           
+            sheet.InsertColumn(1, widthArr.Length);
+            sheet.InsertRow(1, heightArr.Length);
+            for (int j = 1; j <= widthArr.Length; j++) {
+                sheet.Column(j).Width = widthArr[j - 1];
+            }
+            
+            for (int j = 1; j <= heightArr.Length; j++) {
+                sheet.Row(j + 2).Height = heightArr[j - 1];
+            }
+            sheet.Cells[1, 1, 1, 24].Merge = true;
+            sheet.Cells[2, 1, 2, 24].Merge = true;
+            for (int row = 1; row <= table.NumberOfRows; row++) {
+                for (int column = 1; column <= table.NumberOfColumns; column++) {
+                    var cell = sheet.Cells[row, column];
+                    cell.Style.Font.Name = "宋体";
+                    cell.Style.Font.Size = 14;
+                    cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    sheet.Cells[row, column].Value = table.GetValue(row, column);
+                }
+            }
+        }
+        ep.SaveAs(output);
+    }
+}
---
+++ Assets/FileFormat/Editor/MyEditor.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class Excel4Unity : Editor {
+    [MenuItem("阿尔法/自动出题", false, 11)]
+    private static void SetCalcExcel() {
+        string filePath = @$"{Application.dataPath}\abc.xlsx";
+        if (File.Exists(filePath)) {
+            Excel xls = ExcelHelper.LoadExcel(filePath);
+            for (int j = 0; j < xls.Tables.Count; j++) {
+                var table = xls.Tables[j];
+                if (table.TableName.Contains("加减法")) {
+                    var starRow = 3; //从第三行开始
+                    var maxNum = 20;
+                    for (int i = starRow; i < starRow + 25; i++) {
+                        var calcArr = GetCaleArr();
+                        table.SetValue(i, 1, calcArr[0]);
+                        table.SetValue(i, 2, calcArr[3]);
+                        table.SetValue(i, 3, calcArr[1]);
+                        table.SetValue(i, 4, "=");
+                        table.SetValue(i, 5, calcArr[2]);
+
+                        calcArr = GetCaleArr();
+                        table.SetValue(i, 7, calcArr[0]);
+                        table.SetValue(i, 8, calcArr[3]);
+                        table.SetValue(i, 9, calcArr[1]);
+                        table.SetValue(i, 10, "=");
+                        table.SetValue(i, 11, calcArr[2]);
+
+                        calcArr = GetCaleArr();
+                        table.SetValue(i, 13, calcArr[0]);
+                        table.SetValue(i, 14, calcArr[3]);
+                        table.SetValue(i, 15, calcArr[1]);
+                        table.SetValue(i, 16, "=");
+                        table.SetValue(i, 17, calcArr[2]);
+
+                        calcArr = GetCaleArr();
+                        table.SetValue(i, 19, calcArr[0]);
+                        table.SetValue(i, 20, calcArr[3]);
+                        table.SetValue(i, 21, calcArr[1]);
+                        table.SetValue(i, 22, "=");
+                        table.SetValue(i, 23, calcArr[2]);
+                    }
+                }
+            }
+
+            Debug.Log("自动出题完成");
+            ExcelHelper.SaveExcel(xls, filePath);
+        } else {
+            Debug.Log("SetCalcExcel 不存在" + filePath);
+        }
+    }
+
+    private static string[] GetCaleArr() {
+        var emptyNum = Random.Range(0, 3);
+        var symbolVal = Random.Range(0, 2) == 0 ? "+" : "-";
+        var valArr = new[] { "", "", "", symbolVal };
+        valArr[emptyNum] = "(  )";
+        if (emptyNum == 0) {
+            valArr[1] = Random.Range(0, 10).ToString();
+            if (symbolVal == "+") {
+                valArr[2] = Random.Range(int.Parse(valArr[1]), 11).ToString();
+            } else {
+                valArr[2] = Random.Range(0, 11 - int.Parse(valArr[1])).ToString();
+            }
+        } else if (emptyNum == 1) {
+            if (symbolVal == "+") {
+                valArr[0] = Random.Range(0, 10).ToString();
+                valArr[2] = (Random.Range(int.Parse(valArr[0]), 11)).ToString();
+            } else {
+                valArr[0] = Random.Range(5, 10).ToString();
+                valArr[2] = Random.Range(0, int.Parse(valArr[0]) + 1).ToString();
+            }
+        } else if (emptyNum == 2) {
+            if (symbolVal == "+") {
+                valArr[0] = Random.Range(5, 10).ToString();
+                valArr[1] = Random.Range(5, 10).ToString();
+            } else {
+                valArr[0] = Random.Range(5, 10).ToString();
+                valArr[1] = (Random.Range(0, int.Parse(valArr[0]) + 1)).ToString();
+            }
+        }
+
+        return valArr;
+    }
+}
